42a8db1822477dc3e3f578ddbff000b93087ecef
Peter Goshulak <pgoshulak@gmail.com>
2018-04-12 17:59:43 -0400

    Implement enums for boardspaces; initialize 10x10 board

--- scripts/store.js
+++ scripts/store.js
@@ -1,7 +1,34 @@
-class Board {
+const SHIP = {
+  NONE: 0,
+  CRUISER: 1,
+  SUBMARINE: 2,
+  DESTROYER: 3,
+  BATTLESHIP: 4,
+  CARRIER: 5
+};
+
+const STATUS = {
+  EMPTY: 0,
+  ALIVE: 1,
+  HIT: 2,
+  SUNK: 3
+};
+
+class BoardSpace {
+  constructor() {
+    this.ship = SHIP.NONE;
+    this.status = STATUS.EMPTY;
+  }
+}
+class PlayerBoard {
   constructor(playerNum, spaces) {
     this.playerNum = playerNum;
-    this.spaces = spaces;
+    if (!spaces) {
+      this.spaces = [...Array(10).fill(Array(10).fill(new BoardSpace))];
+      console.log(this.spaces);
+    } else {
+      this.spaces = spaces;
+    }
   }
   log() {
     console.log(`hello from player ${this.playerNum}'s board`);
@@ -13,8 +40,8 @@ define((require, exports, module) => {
     state() {
       return {
         playerBoards: [
-          new Board(0, [1, 2, 3]),
-          new Board(1, [4, 5, 6])
+          new PlayerBoard(0),
+          new PlayerBoard(1)
         ],
         currentPlayer: 0,
         currentOpponent: 1
