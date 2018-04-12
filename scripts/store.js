de85ab8504794dabd7596e9f8268dc5107d5fced
Peter Goshulak <pgoshulak@gmail.com>
2018-04-12 18:09:23 -0400

    Add comments to store.js

--- scripts/store.js
+++ scripts/store.js
@@ -1,3 +1,4 @@
+// Constants defining the ship inside a square
 const SHIP = {
   NONE: 0,
   CRUISER: 1,
@@ -7,6 +8,7 @@ const SHIP = {
   CARRIER: 5
 };
 
+// Constants defining a square's status
 const STATUS = {
   EMPTY: 0,
   ALIVE: 1,
@@ -14,19 +16,25 @@ const STATUS = {
   SUNK: 3
 };
 
+// A single square on the board
 class BoardSpace {
   constructor() {
     this.ship = SHIP.NONE;
     this.status = STATUS.EMPTY;
   }
 }
+
+// A player's full 10x10 board
 class PlayerBoard {
   constructor(playerNum, spaces) {
+    // Assign player id
     this.playerNum = playerNum;
     if (!spaces) {
+      // Initialize 10x10 array of empty squares
       this.spaces = [...Array(10).fill(Array(10).fill(new BoardSpace))];
       console.log(this.spaces);
     } else {
+      // Initialize a 10x10 array of predetermined squares
       this.spaces = spaces;
     }
   }
@@ -35,10 +43,11 @@ class PlayerBoard {
   }
 }
 define((require, exports, module) => {
-
   module.exports = {
+    // Full game state
     state() {
       return {
+        // The boards for the two players
         playerBoards: [
           new PlayerBoard(0),
           new PlayerBoard(1)
@@ -48,6 +57,7 @@ define((require, exports, module) => {
       };
     },
 
+    // Swap the boards in the 'opponent' (top) and 'player' (bottom) states
     swapCurrentPlayers(state) {
       let temp = state.currentOpponent;
       state.currentOpponent = state.currentPlayer;
