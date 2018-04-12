88397da18e93fc4db09a74379c10d2943cc6373e
Peter Goshulak <pgoshulak@gmail.com>
2018-04-12 18:13:41 -0400

    Add comments for render.js

--- scripts/render.js
+++ scripts/render.js
@@ -1,18 +1,27 @@
+// Generate JQuery object containing CSS grid of a board
+generateBoard(board) {
+
+}
+
 define((require, exports, module) => {
   module.exports = {
     renderBoards: function (state) {
+      // IDs of the users
       let opponentId = state.currentOpponent;
       let playerId = state.currentPlayer;
 
       let opponentBoard = state.playerBoards[opponentId];
       let playerBoard = state.playerBoards[playerId];
 
+      // Generate JQuery elements with the two boards
       let opponentBoardRendered = $('<div></div>')
         .text(`this is the Opponent board, showing Player ${opponentId}`)
-        .append(`<div>${opponentBoard.spaces.join(', ')}</div>`);
+        .append(generateBoard(opponentBoard));
       let playerBoardRendered = $('<div></div>')
         .text(`this is the Player board, showing Player ${playerId}`)
-        .append(`<div>${playerBoard.spaces.join(', ')}</div>`);
+        .append(generateBoard(playerBoard));
+
+      // Write the boards to the page
       $('#board').empty().append(opponentBoardRendered).append(playerBoardRendered);
     }
   };
