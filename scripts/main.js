ed839cc804f2322a83b6587a193d3fa668b1528f
Peter Goshulak <pgoshulak@gmail.com>
2018-04-12 20:37:31 -0400

    Add refreshBoardBindings() to reinitialize board after swap, etc

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,11 @@ requirejs(['render', 'store'], function (render, store) {
   render.renderBoards(state);
   console.log(state);
 
-  $(document).ready(() => {
-    // Swap the user boards top <-> bottom
-    $('#swapPlayers').click(function () {
-      store.swapCurrentPlayers(state);
-      render.renderBoards(state);
-    });
-    
+  // Various tasks to perform when board is re-rendered
+  const refreshBoardBindings = () => {
     // Get the coordinates of a clicked square
     $('.board-square').click(function () {
+      console.log('click detected');
       let squareCoords = {
         userId: $(this).data('user-id'),
         row: $(this).data('row'),
@@ -19,12 +15,23 @@ requirejs(['render', 'store'], function (render, store) {
       };
       
       store.getSquareInfo(state, squareCoords);
-
+  
       // Test: immediately sets new square info
       store.setSquareInfo(state, squareCoords, {
         ship: 1,
         status: 1
       });
     });
+  };
+
+  $(document).ready(() => {
+    // Swap the user boards top <-> bottom
+    $('#swapPlayers').click(function () {
+      store.swapCurrentPlayers(state);
+      render.renderBoards(state);
+      refreshBoardBindings();
+    });
+    refreshBoardBindings();
+    
   });
 });
