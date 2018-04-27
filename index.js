f2498952742a8d2aa752b3065a815c106baa3efd
Peter Goshulak <pgoshulak@gmail.com>
2018-04-27 18:59:59 -0400

    Implement POST endpoint for scores

--- index.js
+++ index.js
@@ -20,6 +20,33 @@ app.get('/scores', (req, res) => {
   res.send('Error loading scores');
 });
 
+app.post('/scores', (req, res) => {
+  let scoresJson = {};
+  let userName = req.body.userName;
+  // Read scores if they exist
+  if (fs.existsSync(SCORES_DATA)) {
+    scoresJson = JSON.parse(fs.readFileSync(SCORES_DATA, 'utf8'));
+  }
+
+  // Create an entry listing the difference in ships (+2)
+  let score = {
+    scoreDiff: req.body.scoreDiff,
+    timestamp: new Date()
+  };
+
+  // Create the user if doesn't exist
+  if (!scoresJson[userName]) {
+    scoresJson[userName] = [score];
+  } else {
+    scoresJson[userName].push(score);
+  }
+
+  // Write updated scores to file
+  fs.writeFile(SCORES_DATA, JSON.stringify(scoresJson));
+
+  res.send();
+});
+
 app.listen(PORT, () => {
   console.log('Battleship listening on port ' + PORT);
 });
