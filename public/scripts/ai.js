f4a4e8df49a60b59b0a9a042dcec773fc16ceb64
Peter Goshulak <pgoshulak@gmail.com>
2018-04-28 20:50:15 -0400

    Add 'striped' shooting to AI to hunt smallest ship

--- public/scripts/ai.js
+++ public/scripts/ai.js
@@ -6,10 +6,11 @@ function aiGameInit() {
   this.playerBoards[1].playerReady = true;
 }
 
-// Find a random, yet-unshot square
-function getRandomShot() {
+// Find a random, yet-unshot square divisible by smallest living ship
+function getRandomShot(smallestShipLength) {
   let randomShot;
   let status;
+  let isDivisibleByShip;
   do {
     randomShot = {
       userId: 0,
@@ -17,13 +18,18 @@ function getRandomShot() {
       col: Math.floor(Math.random() * 10)
     };
     status = this.getSquareInfo(randomShot).status;
-  } while (status !== STATUS.EMPTY && status !== STATUS.ALIVE);
+
+    // Generate shots that rule out the smallest alive ship
+    // Eg. Patrol Boat is alive, length 2 -> checkerboard (ie max dist between shots is 1 square)
+    // Eg. Battleship is alive, length 4 -> stripes (ie max dist between shots is 3 square)
+    isDivisibleByShip = ((randomShot.row + randomShot.col) % smallestShipLength) === 0;
+  } while (!isDivisibleByShip);
   return randomShot;
 }
 
 // Register that the AI clicked the calculated square
 function aiClick() {
-  this.registerBoardClick(this.getRandomShot());
+  this.registerBoardClick(this.getRandomShot(5));
 }
 
 define((require, exports, module) => {
